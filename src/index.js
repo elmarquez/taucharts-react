8e5a6ed51f3fe53f4c5688021f1db69f2f2e282b
Davis Marques <dmarques@unimelb.edu.au>
2019-06-27 16:18:17 +1000

    Add linting, code formatting support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint no-unused-vars:0 */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Chart } from 'taucharts';
@@ -7,71 +8,70 @@ import equal from 'deep-equal';
  * Wrapper for TauCharts library.
  */
 class TauChartReact extends React.Component {
-	constructor(props) {
-		super(props);
-		this.displayName = 'TauChart';
-	}
+  constructor(props) {
+    super(props);
+    this.displayName = 'TauChart';
+  }
 
-	componentDidMount() {
-		this.renderChart();
-	}
+  componentDidMount() {
+    this.renderChart();
+  }
 
-	componentDidUpdate() {
-		this.chart.destroy();
-		this.renderChart();
-	}
+  componentDidUpdate() {
+    this.chart.destroy();
+    this.renderChart();
+  }
 
-	componentWillUnmount() {
-		this.chart.destroy();
-	}
-
-  /**
-	 * Render the component.
-	 * @return {JSX.Element}
-	 */
-	render() {
-		return (<div style={{height: '100%'}} className={this.props.className} ref="placeholder"></div>)
-	}
+  componentWillUnmount() {
+    this.chart.destroy();
+  }
 
   /**
-	 * Render the chart.
-	 * @return {JSX.Element}
-	 */
-	renderChart() {
-		this.chart = new Chart(
-			Object.assign({}, this.props.options, { data: this.props.data })
-		);
+   * Render the component.
+   * @return {JSX.Element}
+   */
+  render() {
+    return <div style={{ height: '100%' }} className={this.props.className} ref='placeholder'></div>;
+  }
 
-		if (this.props.height && this.props.width) {
-			this.chart.renderTo(this.refs.placeholder, {
-				height: this.props.height,
-				width: this.props.width
-			});
-		} else {
-			this.chart.renderTo(this.refs.placeholder);
-		}
-	}
+  /**
+   * Render the chart.
+   * @return {JSX.Element}
+   */
+  renderChart() {
+    this.chart = new Chart(Object.assign({}, this.props.options, { data: this.props.data }));
 
-	/**
-	 * Determine if the chart should be rerendered.
-	 * @return {boolean}
-	 */
+    if (this.props.height && this.props.width) {
+      this.chart.renderTo(this.refs.placeholder, {
+        height: this.props.height,
+        width: this.props.width,
+      });
+    } else {
+      this.chart.renderTo(this.refs.placeholder);
+    }
+  }
+
+  /**
+   * Determine if the chart should be rerendered.
+   * @return {boolean}
+   */
   shouldComponentUpdate(newProps) {
-		if (newProps.className === this.props.className &&
-			newProps.height === this.props.height &&
-			newProps.width === this.props.width &&
-			equal(newProps.options, this.props.options, { strict: true })
-		) {
-			this.chart.setData(newProps.data);
-			return false;
-		}
-		return true;
-	}
+    if (
+      newProps.className === this.props.className &&
+      newProps.height === this.props.height &&
+      newProps.width === this.props.width &&
+      equal(newProps.options, this.props.options, { strict: true })
+    ) {
+      this.chart.setData(newProps.data);
+      return false;
+    }
+    return true;
+  }
 }
 
 TauChartReact.propTypes = {
-	data: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
-	options: React.PropTypes.object.isRequired,
+  data: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+  options: React.PropTypes.object.isRequired,
 };
 
 export default TauChartReact;
